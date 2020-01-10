b5c2c25666b0d1e9a206c8dfd30d87680ddc2a7f
BuildTools <unconfigured@null.spigotmc.org>
2020-01-10 22:22:29 +0000

    edit

--- src/main/java/jds_project/minecraft/JDCSPlugin.java
+++ src/main/java/jds_project/minecraft/JDCSPlugin.java
@@ -5,10 +5,9 @@ import java.util.Collection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class JDCSPlugin extends JavaPlugin implements Listener {
-	BukkitRunnable backgroundTask;
+	Thread backgroundTask;
 
 	@Override
 	public void onEnable() {
@@ -16,24 +15,24 @@ public class JDCSPlugin extends JavaPlugin implements Listener {
 		this.getCommand("artefact").setExecutor(new CommandKit());
 		getServer().getPluginManager().registerEvents(this, this);
 
-		backgroundTask = new BukkitRunnable() {
-			public void run() {
-				do {
-					Collection<? extends Player> players = getServer().getOnlinePlayers();
-					for (Player player : players) {
-						System.err.println(player);
-					}
-					try {
-						Thread.sleep(3000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} while (true);
-			}
-		};
-		backgroundTask.run();
+		backgroundTask = new Thread(new Runnable() {
 
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				Collection<? extends Player> players = getServer().getOnlinePlayers();
+				for (Player player : players) {
+					System.err.println(player);
+				}
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		backgroundTask.start();
 	}
 
 	@Override
