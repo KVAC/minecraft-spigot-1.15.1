71d8a8827188dc16c6636a338d8390a12bd7c670
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 15:18:36 +0000

    edit

--- src/main/java/jds_project/minecraft/JDCSPlugin.java
+++ src/main/java/jds_project/minecraft/JDCSPlugin.java
@@ -1,40 +1,31 @@
 package jds_project.minecraft;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import jds_project.minecraft.threads.ArtefactInventTask;
 
 public class JDCSPlugin extends JavaPlugin implements Listener {
 	ArtefactInventTask artefactInventTask;
-	JDCSPlugin plugin = this;
+	JDCSPlugin plugin;
 
 	@Override
 	public void onEnable() {
+		startBackInventory();
 		this.getCommand("artefact").setExecutor(new CommandKit());
+
 		getServer().getPluginManager().registerEvents(this, this);
-		checkAndInitBackInvent();
 	}
 
-	@Override
-	public void onLoad() {
-		checkAndInitBackInvent();
+	@EventHandler
+	private void name(PlayerInteractEvent event) {
+
 	}
 
 	@Override
 	public void onDisable() {
-		stopBackInventory();
-	}
-
-	private void checkAndInitBackInvent() {
-		stopBackInventory();
-		startBackInventory();
-	}
-
-	private void stopBackInventory() {
-		if (artefactInventTask != null) {
-			artefactInventTask.cancel();
-		}
 	}
 
 	private void startBackInventory() {
