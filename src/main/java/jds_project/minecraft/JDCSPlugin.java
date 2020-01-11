3c5ded4346a19efeb198d294b055ccbda7da3548
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 07:12:18 +0000

    edit

--- src/main/java/jds_project/minecraft/JDCSPlugin.java
+++ src/main/java/jds_project/minecraft/JDCSPlugin.java
@@ -1,117 +1,43 @@
 package jds_project.minecraft;
 
-import java.io.File;
-import java.io.InputStream;
-import java.util.List;
-import java.util.logging.Logger;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
-import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import jds_project.minecraft.threads.ArtefactInventTask;
 
 public class JDCSPlugin extends JavaPlugin implements Listener {
 	public static Thread backgroundTask;
 
 	@Override
-	public PluginCommand getCommand(String name) {
-		// TODO Auto-generated method stub
-		return super.getCommand(name);
-	}
-
-	@Override
-	public FileConfiguration getConfig() {
-		// TODO Auto-generated method stub
-		return super.getConfig();
-	}
-
-	@Override
-	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
-		// TODO Auto-generated method stub
-		return super.getDefaultWorldGenerator(worldName, id);
-	}
-
-	@Override
-	protected File getFile() {
-		// TODO Auto-generated method stub
-		return super.getFile();
-	}
-
-	@Override
-	public Logger getLogger() {
-		// TODO Auto-generated method stub
-		return super.getLogger();
-	}
-
-	@Override
-	public InputStream getResource(String filename) {
-		// TODO Auto-generated method stub
-		return super.getResource(filename);
-	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Auto-generated method stub
-		return super.onCommand(sender, command, label, args);
-	}
-
-	@Override
-	public void onDisable() {
-		// TODO Auto-generated method stub
-		super.onDisable();
+	public void onEnable() {
+		checkAndInitBackInventory();
+		System.err.println(11111111111l);
+		super.onEnable();
+		this.getCommand("artefact").setExecutor(new CommandKit());
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@Override
 	public void onLoad() {
-		System.err.println(12312323);
-		System.exit(0);
-		super.onLoad();
+		checkAndInitBackInventory();
 	}
 
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		// TODO Auto-generated method stub
-		return super.onTabComplete(sender, command, alias, args);
+	public void onDisable() {
+		if (backgroundTask != null) {
+			backgroundTask.interrupt();
+		}
+		backgroundTask = null;
 	}
 
-	@Override
-	public void reloadConfig() {
-		// TODO Auto-generated method stub
-		super.reloadConfig();
-	}
+	private void checkAndInitBackInventory() {
+		if (backgroundTask != null) {
+			System.exit(0);
+			backgroundTask.interrupt();
+			backgroundTask = null;
+		}
+		backgroundTask = new ArtefactInventTask(this);
 
-	@Override
-	public void saveConfig() {
-		// TODO Auto-generated method stub
-		super.saveConfig();
-	}
-
-	@Override
-	public void saveDefaultConfig() {
-		// TODO Auto-generated method stub
-		super.saveDefaultConfig();
-	}
-
-	@Override
-	public void saveResource(String resourcePath, boolean replace) {
-		// TODO Auto-generated method stub
-		super.saveResource(resourcePath, replace);
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-
-	@Override
-	public void onEnable() {
-		super.onEnable();
-		this.getCommand("artefact").setExecutor(new CommandKit());
-		getServer().getPluginManager().registerEvents(this, this);
 	}
 
 }
--- src/main/java/jds_project/minecraft/threads/ArtefactInventTask.java
+++ src/main/java/jds_project/minecraft/threads/ArtefactInventTask.java
@@ -1,12 +1,42 @@
 package jds_project.minecraft.threads;
 
+import java.util.Collection;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+
+import jds_project.minecraft.JDCSPlugin;
+import jds_project.minecraft.objects.artefacts.Sluda;
+
 public class ArtefactInventTask extends Thread implements Runnable {
-	public ArtefactInventTask() {
+	JDCSPlugin plugin;
+
+	public ArtefactInventTask(JDCSPlugin jdcsPlugin) {
+		this.plugin = jdcsPlugin;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
+		do {
+			Collection<? extends Player> players = plugin.getServer().getOnlinePlayers();
+			for (Player player : players) {
+				PlayerInventory inventory = player.getInventory();
+				if (inventory.contains(Material.STONE)) {
+					Sluda sludaItem = new Sluda();
+					inventory.addItem(sludaItem);
+					sludaItem = null;
+				}
+				inventory = null;
+				player = null;
+			}
+
+			players = null;
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} while (true);
 	}
 }
